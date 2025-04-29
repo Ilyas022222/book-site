0d97b491baa58e0a2399fdd5dbc9cb617b6862ea
ilyas <madaobloom02@gmail.com>
2025-04-29 16:29:18 +0100

    Add files via upload
    - fixed bugs 
    - added css

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const books = [
     cover: "https://m.media-amazon.com/images/I/61sXBXmAWML._AC_UF1000,1000_QL80_.jpg",
     language: "english",
     price: 20.99,
-    publisher: "Bloomsbury Publishing",
+    publisher: "Scholastic Paperbacks",
     link: "https://www.amazon.com/dp/0439785960"
   },
   {
@@ -37,7 +37,7 @@ const books = [
     language: "english",
     price: 7.99,
     publisher: "Independently Published",
-    link: "https://www.amazon.com/dp/198102152X"
+    link: "https://www.amazon.co.uk/Hamlet-William-Shakespeare/dp/B09JJJ7234/ref=pd_vtp_h_pd_vtp_h_d_sccl_5/259-6327440-0924111?pd_rd_w=8uaFC&content-id=amzn1.sym.27b8d297-93e8-4664-82f7-98afee526b43&pf_rd_p=27b8d297-93e8-4664-82f7-98afee526b43&pf_rd_r=GCRVRZYRHY51J4FMSJGJ&pd_rd_wg=ebkGJ&pd_rd_r=5b447172-b800-46e5-ac21-e7812148dc46&pd_rd_i=B09JJJ7234&psc=1"
   },
   {
     title: "The Hunger Games Complete Trilogy",
@@ -76,7 +76,7 @@ const books = [
     language: ["english", "spanish"],
     price: 4.85,
     publisher: "Independently Published",
-    link: "https://www.amazon.com/dp/B0B3Y9Z8X8"
+    link: "https://www.amazon.co.uk/Spanish-Bedtime-Stories-Kids-Collection/dp/B0C47LSG9G/ref=sr_1_3?crid=7CFTEUS426AK&dib=eyJ2IjoiMSJ9.DOIuvKuD0IhrfLBEEUtwWLA6pmvScZg77ZrRh6Dg7_AxyCAC8InTRnwaFVj6AgIngOhsTOvNU3wrKLPZCfdJtzPZ_YEhP97J4oGkcVwfpc7f8_-Xg0x4jlvkmShLdBQO4chKAxAuQGCDzFJrNomgF1Dazn870kYDkLO9GjrMiyNxAEuFOr5UZg7b6-c-udsWyzbdo6dH5e2WVofPqRjVUDLfpxFXjbQU263jrup8Yn0.Cp-tDauXVFrBh-OpmVJpsIF5QpOFFDy6_2SUYUMI3xA&dib_tag=se&keywords=12+Spanish+Bedtime+Stories+for+Kids&qid=1745871758&sprefix=12+spanish+bedtime+stories+for+kids%2Caps%2C67&sr=8-3"
   },
   {
     title: "Routledge Intensive Dutch Course",
@@ -89,7 +89,7 @@ const books = [
     language: "dutch",
     price: 47.99,
     publisher: "Routledge",
-    link: "https://www.amazon.com/dp/041526191X"
+    link: "https://www.amazon.com/Routledge-Intensive-Course-Language-Courses/dp/0415261910/ref=sr_1_1?dib=eyJ2IjoiMSJ9.X2enr-b_8dw9Q6YibHWyKNIo-NiaA8Y8v7csredcwfs.s7XBR36pJzP8UZ7vEyeUA1xjRCYRELSr5BPS7zNbBnw&dib_tag=se&keywords=Routledge+Intensive+Dutch+Course&qid=1745871828&sr=8-1"
   },
   {
     title: "Easy Portuguese Phrase Book",
@@ -102,7 +102,7 @@ const books = [
     language: "portuguese",
     price: 7.99,
     publisher: "Lingo Mastery",
-    link: "https://www.amazon.com/dp/B08XQZ7X8H"
+    link: "https://www.amazon.com/Easy-Portuguese-Phrase-Book-Everyday/dp/1951949374/ref=sr_1_1?crid=29RIEPSEUHA0P&dib=eyJ2IjoiMSJ9.S8DHHAXcjdXvv4m6tC3CH_3rk_VptNxhsUaqIaIQnbCo0eXyCgKWWipu__OFTQICvo17BmhZabRl8VEooQCWKvg-sMT9gslPvSyk7CUFKG2NT3xk8oZ0k1sAy502PxQDsNviImDhsSzVIiS_FXsBgvlwNVpKIKGWvXJaJh5mfNkIkIoEQWe-XUoXYUYNhLWelJ3qAE-cguLnGzc_Jio2G7WTx9QEVYwpzA8OoVlbK74.i7zRnr2XlRxPu0KFnfXAwQBwc1crN89YLkqo-l9cDOM&dib_tag=se&keywords=Easy+Portuguese+Phrase+Book&qid=1745871909&sprefix=easy+portuguese+phrase+book%2Caps%2C198&sr=8-1"
   },
   {
     title: "French Short Stories: Nouvelles Francaises",
@@ -115,7 +115,7 @@ const books = [
     language: "french",
     price: 9.75,
     publisher: "Penguin",
-    link: "https://www.amazon.com/dp/0140265430"
+    link: "https://www.amazon.com/Short-Stories-French-Penguin-Parallel/dp/0140265430/ref=sr_1_1?crid=11D3K3NFZUMP8&dib=eyJ2IjoiMSJ9.ukPU4XBe87ya9rTjNnCIlFKgGS3gIJltYyY3a1Qw5AB3sWicppUOef_MNxrohLfnCMKrAjgnkRpnN9tfHQ7J8NmyWkpLrDWU2HmKNkX0bJ8._nvhug7glC5sYVNuyFtDm04UYpmMQswzsPOVgBtqO9c&dib_tag=se&keywords=Short+Stories+in+French%3A+New+Penguin+Parallel+Text&qid=1745871947&s=books&sprefix=short+stories+in+french+new+penguin+parallel+text%2Cstripbooks-intl-ship%2C119&sr=1-1"
   },
   {
     title: "Home Before Dark",
@@ -128,11 +128,27 @@ const books = [
     language: "english",
     price: 6.35,
     publisher: "Hodder Paperbacks",
-    link: "https://www.amazon.com/dp/1529359982"
+    link: "https://www.amazon.com/Home-Before-Dark-Riley-Sager/dp/1524745197/ref=sr_1_2?crid=3VTX69K4MLYOM&dib=eyJ2IjoiMSJ9.V8fw-YH309UttAmrRSK2wMEwUenXMeJUK-PqrYud-_nmkFdp9Ho1mZ7zTjijScrCaJ3TLjU5MY1Ot3SkzgLJhFtDUl_7Vw13Wb00ojckuE1wJj5VX4cb4FMLZr7M3xB2RwNXlaT98fN2L6OnoDwrZePU75eUuvj90a6tTm06y4QDfr5sojg1KJolKLg25JY0jCLTdGiBLTBnFe-rYg_Vvv_aPi5mqhtHSlIWycdEhuc28UYJD4_iJQ4SAWykJmWI2Kwv86CXMIOo7qu0ErLmHEgNuxWGTH9Tl_LLvjJQjv8.TUjK69eB5Ie47hDNR8Dw0ac7keGD8YEwZqPM5fD1-sw&dib_tag=se&keywords=Home+Before+Dark&qid=1745872025&sprefix=home+before+dark%2Caps%2C170&sr=8-2"
   }
 ];
 
-let wishlist = [];
+// Load wishlist from local storage
+wishlist = JSON.parse(localStorage.getItem('wishlist')) || [];
+
+function addToWishlist(title) {
+  if (!wishlist.includes(title)) {
+    wishlist.push(title);
+    localStorage.setItem('wishlist', JSON.stringify(wishlist));
+    alert(`"${title}" added to your wishlist!`);
+    displayBooks(currentBooks);
+  }
+}
+document.getElementById('viewWishlistBtn').addEventListener('click', function(e) {
+  e.preventDefault();
+  currentBooks = books.filter(book => wishlist.includes(book.title));
+  displayBooks(currentBooks);
+});
+
 
 // Add event listener to open book link on click
 document.addEventListener('click', function(e) {
@@ -144,6 +160,7 @@ document.addEventListener('click', function(e) {
     }
   }
 });
+
 
 function displayBooks(bookList) {
   const results = document.getElementById('results');
